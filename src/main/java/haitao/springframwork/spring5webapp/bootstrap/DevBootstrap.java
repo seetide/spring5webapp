a1e5b9f843a4b15759eb1ebd3e9778b97873112f
it22 <it7@kystationtemp.localdomain>
2019-10-23 09:32:21 -0400

    add publisher

--- src/main/java/haitao/springframwork/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/haitao/springframwork/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package haitao.springframwork.spring5webapp.bootstrap;
 
 import haitao.springframwork.spring5webapp.model.Author;
 import haitao.springframwork.spring5webapp.model.Book;
+import haitao.springframwork.spring5webapp.model.Publisher;
 import haitao.springframwork.spring5webapp.repositories.AuthorRepository;
 import haitao.springframwork.spring5webapp.repositories.BookRepository;
+import haitao.springframwork.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -17,22 +19,29 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         initData();
     }
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     private void initData() {
 
+        Publisher publisher = new Publisher();
+        publisher.setName("foo");
+
+        publisherRepository.save(publisher);
+
         //Eric
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("domain Driven Design", "1234", "Harper Collins");
+        Book ddd = new Book("domain Driven Design", "1234", publisher);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
@@ -40,7 +49,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
         bookRepository.save(ddd);
         // Rod
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "234444", "Worx");
+        Book noEJB = new Book("J2EE Development without EJB", "234444", publisher);
         rod.getBooks().add(noEJB);
 
         authorRepository.save(rod);
